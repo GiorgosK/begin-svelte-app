2b23fca61a6421a57ee063f6d5df11c417c7b245
Giorgos Kontopoulos <kongeo@gmail.com>
2020-05-30 17:45:40 +0300

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -12,7 +12,7 @@
 <main>
   <h1>Hello {name}!</h1>
   <h2>{message}</h2>
-  <h3>Change me!</h3>
+  <h3>Change me to force rebuild!</h3>
   <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
